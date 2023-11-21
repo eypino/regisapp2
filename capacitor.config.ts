f5ffbd1a9dd7a5833f1cf08643a2df1380d5a5f5
eypino <107155607+eypino@users.noreply.github.com>
2023-11-21 20:25:44 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.regisapp2',
+  appName: 'regisapp2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
